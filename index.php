c7d0bc357e8773692bb8107ff1c5ddbd3ce97ce2
marinee09 <40633491+marinee09@users.noreply.github.com>
2018-07-23 13:58:59 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,47 @@
+<?php 
+$name = "Marine"; // variable
+echo $name . "<br>"; //affiche
+//1
+
+$lastname = "Di pietro" . "<br>";
+$firstname = "Rose";
+$age = 27;
+echo $lastname . " " . $firstname . " " . $age . "<br>";
+//2
+
+$km = 1;
+echo $km . "<br>";
+$km = 3;
+echo $km . "<br>";
+$km = 125;
+echo $km . "<br>";
+//3
+
+$string = "lol" . "<br>";
+$int = "10" . "<br>";
+$float = "1.5" . "<br>";
+$bool = true;
+echo $string . " " . $int . " " . $float . " " . $bool . "<br>";
+//4
+
+$nul = null;
+echo $nul;
+$nul = "20" . "<br>";
+echo $nul;
+//5
+
+$name = "Loic";
+echo "Bonjour" . " " . $name . " " . "comment vas-tu ?" . "<br>";
+//6
+
+$lastname = "Picard";
+$firstname = "Marion";
+$age = 27;
+echo "Bonjour" . " " . $lastname . " " . $firstname . " " . "tu as" . " " . $age . " " . "ans" . "<br>";
+//7
+
+$sept = (3+4);
+$cent = (5*20);
+$neuf = (45/5);
+echo $sept . "<br>" . $cent . "<br>" . $neuf . "<br>";
+//8
